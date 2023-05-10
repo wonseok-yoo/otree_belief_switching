e8d550b8cff4e4b1e5ae7f6df7e612bfcf709354
wonseok-yoo <61504545+wonseok-yoo@users.noreply.github.com>
2023-05-10 17:37:38 -0400

    Update __init__.py

--- belief_project/Task0/__init__.py
+++ belief_project/Task0/__init__.py
@@ -47,6 +47,8 @@ class Group(BaseGroup):
 
 
 class Player(BasePlayer):
+    belief_dummy_1 = models.FloatField()
+    belief_dummy_2 = models.FloatField()
     prob_box1 = models.FloatField()
     prob_box2 = models.FloatField()
     ecu = models.FloatField(label = 'Belief Updating Reward', initial = 0)
@@ -72,9 +74,26 @@ class Player(BasePlayer):
 class Introduction(Page):
     def is_displayed(player):
         return player.round_number == 1
+    
 class Introduction_2(Page):
     def is_displayed(player):
         return player.round_number == 1
+    
+class Practice(Page):    
+    form_model = 'player'
+    form_fields = ['belief_dummy_1']
+    def is_displayed(player):
+        player.round_number == 1
+
+class Practice_2(Page): 
+    form_model = 'player'
+    form_fields = ['belief_dummy_2']
+    def vars_for_template(player):
+        previous = player.belief_dummy_1
+        return {'previous' : previous}    
+    def is_displayed(player):
+        player.round_number == 1
+        
 
 class Instructions_0(Page):
     def is_displayed(player):
@@ -193,6 +212,8 @@ class Payoff(Page):
 
 page_sequence = [
     Introduction,
+    Practice,
+    Practice_2,
     Instructions_0,
     Instructions,
     BoxEndowment,
