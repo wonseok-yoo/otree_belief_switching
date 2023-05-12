ec757c6de458f133b0adc0f25fb31accb09419fb
wonseok-yoo <61504545+wonseok-yoo@users.noreply.github.com>
2023-05-12 10:36:36 -0400

    Update __init__.py

--- belief_project/Task2/__init__.py
+++ belief_project/Task2/__init__.py
@@ -203,6 +203,11 @@ class Payoff(Page):
         return {'payoff': player.payoff}
     def is_displayed(player):
         return player.round_number == Constants.num_rounds
+    
+class Wait(WaitPage):
+    wait_for_all_groups = True
+    def is_displayed(player):
+        return player.round_number == Constants.num_rounds
 
 
 page_sequence = [
@@ -214,4 +219,5 @@ page_sequence = [
     Decision,
     NextTask,
     Payoff,
+    Wait,
 ]
